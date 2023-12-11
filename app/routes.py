cba6fe16e0c473e327d0f7a2a9a03d551706b34e
tgatling <77751040+tgatling@users.noreply.github.com>
2023-12-11 14:39:43 -0500

    added home page which will direct user to add a book or display books. fix button style on the book display page. (#5)
    Co-authored-by: tgatling <tequesha.l.gatling@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -6,94 +6,97 @@ import logging
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 
-@app.route('/add_book', methods=['POST'])
+@app.route('/add_book', methods=['GET', 'POST'])
 def add_book():
-    cursor = db.cursor()
+    if request.method == 'POST':
+        cursor = db.cursor()
 
-    try:
-        title = request.form['title']
-        read_status = int(request.form.get('read_status', 0))  # Convert to integer (1 for "Read", 0 for "Unread")
-        isbn = request.form.get('isbn', None)
-        description = request.form.get('description', None)
-        image_url = request.form.get('image_url', None)
-        external_url = request.form.get('external_url', None)
+        try:
+            title = request.form['title']
+            read_status = int(request.form.get('read_status', 0))  # Convert to integer (1 for "Read", 0 for "Unread")
+            isbn = request.form.get('isbn', None)
+            description = request.form.get('description', None)
+            image_url = request.form.get('image_url', None)
+            external_url = request.form.get('external_url', None)
 
-        # AUTHOR
-        # Check if the user selected an existing author or is adding a new one
-        if 'existing_author' in request.form:
-            author_id = request.form['existing_author']
-        else:
-            # If adding a new author, insert into authors table
-            author_first_name = request.form['author_first_name']
-            author_last_name = request.form['author_last_name']
+            # AUTHOR
+            # Check if the user selected an existing author or is adding a new one
+            if 'existing_author' in request.form:
+                author_id = request.form['existing_author']
+            else:
+                # If adding a new author, insert into authors table
+                author_first_name = request.form['author_first_name']
+                author_last_name = request.form['author_last_name']
 
-            # Print debug
-            logging.debug(f"In function add_books: Trying to get author_id for: {author_first_name} {author_last_name}")
+                # Print debug
+                logging.debug(f"In function add_books: Trying to get author_id for: {author_first_name} {author_last_name}")
 
-            author_id = get_author_id(author_first_name, author_last_name)
-            logging.debug(f"In function add_books: Retrieved author_id: {author_id}")
+                author_id = get_author_id(author_first_name, author_last_name)
+                logging.debug(f"In function add_books: Retrieved author_id: {author_id}")
 
-            if not author_id:
-                cursor.execute(
-                    "INSERT INTO authors (first_name, last_name) VALUES (%s, %s)",
-                    (author_first_name, author_last_name)
-                )
-                db.commit()
+                if not author_id:
+                    cursor.execute(
+                        "INSERT INTO authors (first_name, last_name) VALUES (%s, %s)",
+                        (author_first_name, author_last_name)
+                    )
+                    db.commit()
 
-                # Retrieve the newly inserted author's ID
-                cursor.execute("SELECT LAST_INSERT_ID()")
-                author_id = cursor.fetchone()[0]
-                logging.debug(f"In function add_books: Inserted author_id: {author_id}")
+                    # Retrieve the newly inserted author's ID
+                    cursor.execute("SELECT LAST_INSERT_ID()")
+                    author_id = cursor.fetchone()[0]
+                    logging.debug(f"In function add_books: Inserted author_id: {author_id}")
 
-        # GENRE
-        # Check if the user selected an existing genre or is adding a new one
-        if 'existing_genre' in request.form:
-            genre_id = request.form['existing_genre']
-        else:
-            # If adding a new genre, insert into genres table
-            new_genre = request.form.get('new_genre', None)
-            genre_id = get_genre_id(new_genre)
-            if not genre_id:
-                cursor.execute(
-                    "INSERT INTO genres (genre_name) VALUES (%s)",
-                    (new_genre,)
-                )
-                db.commit()
+            # GENRE
+            # Check if the user selected an existing genre or is adding a new one
+            if 'existing_genre' in request.form:
+                genre_id = request.form['existing_genre']
+            else:
+                # If adding a new genre, insert into genres table
+                new_genre = request.form.get('new_genre', None)
+                genre_id = get_genre_id(new_genre)
+                if not genre_id:
+                    cursor.execute(
+                        "INSERT INTO genres (genre_name) VALUES (%s)",
+                        (new_genre,)
+                    )
+                    db.commit()
 
-                # Retrieve the newly inserted genre's ID
-                cursor.execute("SELECT LAST_INSERT_ID()")
-                genre_id = cursor.fetchone()[0]
-                logging.debug(f"In function add_books: genre_id: {genre_id}")
+                    # Retrieve the newly inserted genre's ID
+                    cursor.execute("SELECT LAST_INSERT_ID()")
+                    genre_id = cursor.fetchone()[0]
+                    logging.debug(f"In function add_books: genre_id: {genre_id}")
 
-        # MEDIA
-        # Get media_id based on media_name
-        media = request.form['media']
-        media_id = get_media_id(media)
-        logging.debug(f"In function add_books: Selected Media ID: {media_id}")
+            # MEDIA
+            # Get media_id based on media_name
+            media = request.form['media']
+            media_id = get_media_id(media)
+            logging.debug(f"In function add_books: Selected Media ID: {media_id}")
 
-        # Insert book into the database
-        query = """
-            INSERT INTO books 
-            (title, author_id, read_status, isbn, description, image_url, external_url, media_id, genre_id) 
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """
-        values = (title, author_id, read_status, isbn, description, image_url, external_url, media_id, genre_id)
-        logging.debug(f"In function add_books: values: {values}")
+            # Insert book into the database
+            query = """
+                INSERT INTO books 
+                (title, author_id, read_status, isbn, description, image_url, external_url, media_id, genre_id) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+            values = (title, author_id, read_status, isbn, description, image_url, external_url, media_id, genre_id)
+            logging.debug(f"In function add_books: values: {values}")
 
-        cursor.execute(query, values)
-        db.commit()
-        logging.info(f"In function add_books: Book added successfully! {values}")
-    except Exception as e:
-        # An error occurred, rollback the transaction and handle the error
-        db.rollback()
-        logging.error(f"In function add_books: Error: {str(e)}")
-    finally:
-        cursor.close()
-    return redirect(url_for('index'))
+            cursor.execute(query, values)
+            db.commit()
+            logging.info(f"In function add_books: Book added successfully! {values}")
+        except Exception as e:
+            # An error occurred, rollback the transaction and handle the error
+            db.rollback()
+            logging.error(f"In function add_books: Error: {str(e)}")
+        finally:
+            cursor.close()
+        return redirect(url_for('home'))
+
+    return render_template('add_book.html')
 
 
 @app.route('/display_books')
