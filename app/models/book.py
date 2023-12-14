856624bc08974f1cfb0826575eb81dc0534a9a44
tgatling <tequesha.l.gatling@gmail.com>
2023-12-14 12:54:22 -0500

    corrected book model to fit the changes in the database. syntax error causing the books not to appear under medium tab. corrected.

--- app/models/book.py
+++ app/models/book.py
@@ -1,12 +1,12 @@
 class Book:
-    def __init__(self, book_id, title, author_first_name, author_last_name, read_status, media, isbn, description,
+    def __init__(self, book_id, title, author_first_name, author_last_name, read_status, medium, isbn, description,
                  image_url, external_url, genre):
         self.book_id = book_id
         self.title = title
         self.author_first_name = author_first_name
         self.author_last_name = author_last_name
         self.read_status = read_status
-        self.media = media
+        self.medium = medium
         self.isbn = isbn
         self.description = description
         self.image_url = image_url
